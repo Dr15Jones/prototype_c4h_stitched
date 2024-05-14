cms-sw/cmssw

Generated cfi files now may use new module syntax
- denote if must use full type info when dumping to cfi
- test module now makes explicit named cfi files compatible with default

FWCore/ParameterSet/interface/ParameterSwitch.h
@@ -86,13 +86,29 @@ namespace edm {
       }
     }
 
-    void writeCfi_(
-        std::ostream& os, bool optional, bool& startWithComma, int indentation, bool& wroteSomething) const override {
-      switch_.writeCfi(os, optional, startWithComma, indentation, wroteSomething);
+    void writeCfi_(std::ostream& os,
+                   bool optional,
+                   bool& startWithComma,
+                   int indentation,
+                   CfiOptions& options,
+                   bool& wroteSomething) const override {
+      switch_.writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
+
+      if (std::holds_alternative<cfi::Default>(options)) {
+        std::set<std::string> labels;
+        std::set<ParameterTypes> parameterTypes;
+        std::set<ParameterTypes> wildcardTypes;
+        for (auto const& n : cases_) {
+          n.second->checkAndGetLabelsAndTypes(labels, parameterTypes, wildcardTypes);
+        }
+        for (auto const& l : labels) {
+          cfi::pathMustBeFull(options, l);
+        }
+      }
 
       typename CaseMap::const_iterator selectedCase = cases_.find(switch_.getDefaultValue());
       if (selectedCase != cases_.end()) {
-        selectedCase->second->writeCfi(os, optional, startWithComma, indentation, wroteSomething);
+        selectedCase->second->writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
       }
     }
 

FWCore/ParameterSet/interface/PluginDescription.h
@@ -131,16 +131,25 @@ namespace edm {
       validatedLabels.insert(n.begin(), n.end());
     }
 
-    void writeCfi_(
-        std::ostream& os, bool optional, bool& startWithComma, int indentation, bool& wroteSomething) const final {
+    void writeCfi_(std::ostream& os,
+                   bool optional,
+                   bool& startWithComma,
+                   int indentation,
+                   CfiOptions& options,
+                   bool& wroteSomething) const final {
       if (not defaultType_.empty()) {
         if (!edmplugin::PluginManager::isAvailable()) {
           auto conf = edmplugin::standard::config();
           conf.allowNoCache();
           edmplugin::PluginManager::configure(conf);
         }
-        loadDescription(defaultType_).writeCfi(os, startWithComma, indentation);
+        //given each plugin can have very different parameters we should do a full dump
+        CfiOptions ops = cfi::Full{};
+        loadDescription(defaultType_).writeCfi(os, startWithComma, indentation, ops);
         wroteSomething = true;
+      }
+      if (std::holds_alternative<cfi::Default>(options)) {
+        std::get<cfi::Default>(options).pathMustBeFull();
       }
     }
 

FWCore/ParameterSet/src/ConfigurationDescriptions.cc
@@ -141,33 +141,42 @@ namespace edm {
   }
 
   void ConfigurationDescriptions::writeCfis(std::set<std::string>& usedCfiFileNames) const {
-    for_all(descriptions_,
-            std::bind(&ConfigurationDescriptions::writeCfiForLabel,
-                      std::placeholders::_1,
-                      std::cref(baseType_),
-                      std::cref(pluginName_),
-                      std::ref(usedCfiFileNames)));
+    bool wroteClassFile = false;
+    cfi::Paths paths;
     if (defaultDescDefined_) {
-      writeClassFile(defaultDesc_);
+      paths = writeClassFile(defaultDesc_, not descriptions_.empty());
+      wroteClassFile = true;
     } else if (descriptions_.size() == 1) {
-      writeClassFile(descriptions_.begin()->second);
+      paths = writeClassFile(descriptions_.begin()->second, true);
+      wroteClassFile = true;
+    }
+    CfiOptions ops = wroteClassFile ? CfiOptions{cfi::LabelValue{paths}} : CfiOptions{cfi::Full{}};
+    for (auto& d : descriptions_) {
+      writeCfiForLabel(d, baseType_, pluginName_, 1 == descriptions_.size(), ops, usedCfiFileNames);
     }
   }
 
-  void ConfigurationDescriptions::writeClassFile(ParameterSetDescription const& iDesc) const {
-    std::string pluginName = pluginName_;
-    auto found = pluginName.find("::");
-    while (found != std::string::npos) {
-      pluginName.replace(found, 2, "_");
-      found = pluginName.find("::");
+  namespace {
+    std::string modifyPluginName(std::string iName) {
+      auto found = iName.find("::");
+      while (found != std::string::npos) {
+        iName.replace(found, 2, "_");
+        found = iName.find("::");
+      }
+      //Symbols that can appear in our plugin names but can't in python function names
+      const std::string toReplace("@<>,");
+      found = iName.find_first_of(toReplace);
+      while (found != std::string::npos) {
+        iName.replace(found, 1, "_");
+        found = iName.find_first_of(toReplace, found);
+      }
+      return iName;
     }
-    //Symbols that can appear in our plugin names but can't in python function names
-    const std::string toReplace("@<>,");
-    found = pluginName.find_first_of(toReplace);
-    while (found != std::string::npos) {
-      pluginName.replace(found, 1, "_");
-      found = pluginName.find_first_of(toReplace, found);
-    }
+  }  // namespace
+
+  cfi::Paths ConfigurationDescriptions::writeClassFile(ParameterSetDescription const& iDesc,
+                                                       bool willUseWithCfis) const {
+    std::string pluginName = modifyPluginName(pluginName_);
 
     std::string fileName = pluginName + ".py";
     std::ofstream outFile(fileName.c_str());
@@ -187,7 +196,8 @@ namespace edm {
 
     bool startWithComma = true;
     int indentation = 4;
-    iDesc.writeCfi(outFile, startWithComma, indentation);
+    CfiOptions ops = willUseWithCfis ? CfiOptions{cfi::Default{}} : CfiOptions{cfi::Full{}};
+    iDesc.writeCfi(outFile, startWithComma, indentation, ops);
 
     outFile << ")\n"
                "  for k,v in kwargs.items():\n"
@@ -195,11 +205,14 @@ namespace edm {
                "  return mod\n";
 
     outFile.close();
+    return std::holds_alternative<cfi::Default>(ops) ? std::get<cfi::Default>(ops).releasePaths() : cfi::Paths{};
   }
 
   void ConfigurationDescriptions::writeCfiForLabel(std::pair<std::string, ParameterSetDescription> const& labelAndDesc,
                                                    std::string const& baseType,
                                                    std::string const& pluginName,
+                                                   bool isSameAsDefault,
+                                                   CfiOptions& options,
                                                    std::set<std::string>& usedCfiFileNames) {
     if (0 == strcmp(baseType.c_str(), kService) && labelAndDesc.first != pluginName) {
       throw edm::Exception(edm::errors::LogicError,
@@ -250,13 +263,22 @@ namespace edm {
       throw ex;
     }
 
-    outFile << "import FWCore.ParameterSet.Config as cms\n\n";
-    outFile << labelAndDesc.first << " = cms." << baseType << "('" << pluginName << "'";
-
     bool startWithComma = true;
-    int indentation = 2;
-    labelAndDesc.second.writeCfi(outFile, startWithComma, indentation);
+    if (not writeOnlyLabelValue(options)) {
+      outFile << "import FWCore.ParameterSet.Config as cms\n\n";
+      outFile << labelAndDesc.first << " = cms." << baseType << "('" << pluginName << "'";
+    } else {
+      outFile << "import FWCore.ParameterSet.Config as cms\n\n";
 
+      auto pythonName = modifyPluginName(pluginName);
+      outFile << "from ." << pythonName << " import " << pythonName << "\n\n";
+      outFile << labelAndDesc.first << " = " << pythonName << "(";
+      startWithComma = false;
+    }
+    if (not isSameAsDefault) {
+      int indentation = 2;
+      labelAndDesc.second.writeCfi(outFile, startWithComma, indentation, options);
+    }
     outFile << ")\n";
 
     outFile.close();

FWCore/ParameterSet/src/IfExistsDescription.cc
@@ -72,10 +72,15 @@ namespace edm {
     }
   }
 
-  void IfExistsDescription::writeCfi_(
-      std::ostream& os, bool optional, bool& startWithComma, int indentation, bool& wroteSomething) const {
-    node_left_->writeCfi(os, optional, startWithComma, indentation, wroteSomething);
-    node_right_->writeCfi(os, optional, startWithComma, indentation, wroteSomething);
+  void IfExistsDescription::writeCfi_(std::ostream& os,
+                                      bool optional,
+                                      bool& startWithComma,
+                                      int indentation,
+                                      CfiOptions& options,
+                                      bool& wroteSomething) const {
+    node_left_->writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
+    node_right_->writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
+    pathMustBeFull(options);
   }
 
   void IfExistsDescription::print_(std::ostream& os, bool optional, bool writeToCfi, DocFormatHelper& dfh) const {

FWCore/ParameterSet/src/ParameterSetDescription.cc
@@ -125,20 +125,28 @@ namespace edm {
     }
   }
 
-  void ParameterSetDescription::writeCfi(std::ostream& os, bool startWithComma, int indentation) const {
-    using std::placeholders::_1;
+  void ParameterSetDescription::writeCfi(std::ostream& os,
+                                         bool startWithComma,
+                                         int indentation,
+                                         CfiOptions& options) const {
     bool wroteSomething = false;
 
     bool seenWildcard = false;
+    bool seenMultipleWildcards = false;
     for (auto const& entry : entries_) {
       //only add the first seen wildcard to the cfi. This avoids possible ambiguities.
       if (entry.node()->isWildcard()) {
         if (seenWildcard == true) {
+          seenMultipleWildcards = true;
           continue;
         }
         seenWildcard = true;
       }
-      writeNode(entry, os, startWithComma, indentation, wroteSomething);
+      writeNode(entry, os, startWithComma, indentation, options, wroteSomething);
+    }
+
+    if ((anythingAllowed() or seenMultipleWildcards)) {
+      cfi::pathMustBeFull(options);
     }
 
     if (wroteSomething) {
@@ -227,10 +235,17 @@ namespace edm {
     }
   }
 
-  void ParameterSetDescription::writeNode(
-      SetDescriptionEntry const& entry, std::ostream& os, bool& startWithComma, int indentation, bool& wroteSomething) {
+  void ParameterSetDescription::writeNode(SetDescriptionEntry const& entry,
+                                          std::ostream& os,
+                                          bool& startWithComma,
+                                          int indentation,
+                                          CfiOptions& options,
+                                          bool& wroteSomething) {
     if (entry.writeToCfi()) {
-      entry.node()->writeCfi(os, entry.optional(), startWithComma, indentation, wroteSomething);
+      entry.node()->writeCfi(os, entry.optional(), startWithComma, indentation, options, wroteSomething);
+    } else {
+      //The simplest way to handle this is to force all items to be full in this PSet
+      cfi::pathMustBeFull(options);
     }
   }
 

Dr15Jones/prototype_c4h_stitched

FWCore/ParameterSet/interface/ParameterSwitch.h
@@ -86,13 +86,29 @@ namespace edm {
       }
     }
 
-    void writeCfi_(
-        std::ostream& os, bool optional, bool& startWithComma, int indentation, bool& wroteSomething) const override {
-      switch_.writeCfi(os, optional, startWithComma, indentation, wroteSomething);
+    void writeCfi_(std::ostream& os,
+                   bool optional,
+                   bool& startWithComma,
+                   int indentation,
+                   CfiOptions& options,
+                   bool& wroteSomething) const override {
+      switch_.writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
+
+      if (std::holds_alternative<cfi::ClassFile>(options)) {
+        std::set<std::string> labels;
+        std::set<ParameterTypes> parameterTypes;
+        std::set<ParameterTypes> wildcardTypes;
+        for (auto const& n : cases_) {
+          n.second->checkAndGetLabelsAndTypes(labels, parameterTypes, wildcardTypes);
+        }
+        for (auto const& l : labels) {
+          cfi::parameterMustBeTyped(options, l);
+        }
+      }
 
       typename CaseMap::const_iterator selectedCase = cases_.find(switch_.getDefaultValue());
       if (selectedCase != cases_.end()) {
-        selectedCase->second->writeCfi(os, optional, startWithComma, indentation, wroteSomething);
+        selectedCase->second->writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
       }
     }
 

FWCore/ParameterSet/interface/PluginDescription.h
@@ -131,16 +131,25 @@ namespace edm {
       validatedLabels.insert(n.begin(), n.end());
     }
 
-    void writeCfi_(
-        std::ostream& os, bool optional, bool& startWithComma, int indentation, bool& wroteSomething) const final {
+    void writeCfi_(std::ostream& os,
+                   bool optional,
+                   bool& startWithComma,
+                   int indentation,
+                   CfiOptions& options,
+                   bool& wroteSomething) const final {
       if (not defaultType_.empty()) {
         if (!edmplugin::PluginManager::isAvailable()) {
           auto conf = edmplugin::standard::config();
           conf.allowNoCache();
           edmplugin::PluginManager::configure(conf);
         }
-        loadDescription(defaultType_).writeCfi(os, startWithComma, indentation);
+        //given each plugin can have very different parameters we should do a full dump
+        CfiOptions ops = cfi::Typed{};
+        loadDescription(defaultType_).writeCfi(os, startWithComma, indentation, ops);
         wroteSomething = true;
+      }
+      if (std::holds_alternative<cfi::ClassFile>(options)) {
+        std::get<cfi::ClassFile>(options).parameterMustBeTyped();
       }
     }
 

FWCore/ParameterSet/src/ConfigurationDescriptions.cc
@@ -141,33 +141,42 @@ namespace edm {
   }
 
   void ConfigurationDescriptions::writeCfis(std::set<std::string>& usedCfiFileNames) const {
-    for_all(descriptions_,
-            std::bind(&ConfigurationDescriptions::writeCfiForLabel,
-                      std::placeholders::_1,
-                      std::cref(baseType_),
-                      std::cref(pluginName_),
-                      std::ref(usedCfiFileNames)));
+    bool wroteClassFile = false;
+    cfi::Paths paths;
     if (defaultDescDefined_) {
-      writeClassFile(defaultDesc_);
+      paths = writeClassFile(defaultDesc_, not descriptions_.empty());
+      wroteClassFile = true;
     } else if (descriptions_.size() == 1) {
-      writeClassFile(descriptions_.begin()->second);
+      paths = writeClassFile(descriptions_.begin()->second, true);
+      wroteClassFile = true;
+    }
+    CfiOptions ops = wroteClassFile ? CfiOptions{cfi::Untyped{paths}} : CfiOptions{cfi::Typed{}};
+    for (auto& d : descriptions_) {
+      writeCfiForLabel(d, baseType_, pluginName_, 1 == descriptions_.size(), ops, usedCfiFileNames);
     }
   }
 
-  void ConfigurationDescriptions::writeClassFile(ParameterSetDescription const& iDesc) const {
-    std::string pluginName = pluginName_;
-    auto found = pluginName.find("::");
-    while (found != std::string::npos) {
-      pluginName.replace(found, 2, "_");
-      found = pluginName.find("::");
+  namespace {
+    std::string modifyPluginName(std::string iName) {
+      auto found = iName.find("::");
+      while (found != std::string::npos) {
+        iName.replace(found, 2, "_");
+        found = iName.find("::");
+      }
+      //Symbols that can appear in our plugin names but can't in python function names
+      const std::string toReplace("@<>,");
+      found = iName.find_first_of(toReplace);
+      while (found != std::string::npos) {
+        iName.replace(found, 1, "_");
+        found = iName.find_first_of(toReplace, found);
+      }
+      return iName;
     }
-    //Symbols that can appear in our plugin names but can't in python function names
-    const std::string toReplace("@<>,");
-    found = pluginName.find_first_of(toReplace);
-    while (found != std::string::npos) {
-      pluginName.replace(found, 1, "_");
-      found = pluginName.find_first_of(toReplace, found);
-    }
+  }  // namespace
+
+  cfi::Paths ConfigurationDescriptions::writeClassFile(ParameterSetDescription const& iDesc,
+                                                       bool willUseWithCfis) const {
+    std::string pluginName = modifyPluginName(pluginName_);
 
     std::string fileName = pluginName + ".py";
     std::ofstream outFile(fileName.c_str());
@@ -187,7 +196,8 @@ namespace edm {
 
     bool startWithComma = true;
     int indentation = 4;
-    iDesc.writeCfi(outFile, startWithComma, indentation);
+    CfiOptions ops = willUseWithCfis ? CfiOptions{cfi::ClassFile{}} : CfiOptions{cfi::Typed{}};
+    iDesc.writeCfi(outFile, startWithComma, indentation, ops);
 
     outFile << ")\n"
                "  for k,v in kwargs.items():\n"
@@ -195,11 +205,14 @@ namespace edm {
                "  return mod\n";
 
     outFile.close();
+    return std::holds_alternative<cfi::ClassFile>(ops) ? std::get<cfi::ClassFile>(ops).releasePaths() : cfi::Paths{};
   }
 
   void ConfigurationDescriptions::writeCfiForLabel(std::pair<std::string, ParameterSetDescription> const& labelAndDesc,
                                                    std::string const& baseType,
                                                    std::string const& pluginName,
+                                                   bool isSameAsDefault,
+                                                   CfiOptions& options,
                                                    std::set<std::string>& usedCfiFileNames) {
     if (0 == strcmp(baseType.c_str(), kService) && labelAndDesc.first != pluginName) {
       throw edm::Exception(edm::errors::LogicError,
@@ -250,13 +263,22 @@ namespace edm {
       throw ex;
     }
 
-    outFile << "import FWCore.ParameterSet.Config as cms\n\n";
-    outFile << labelAndDesc.first << " = cms." << baseType << "('" << pluginName << "'";
-
     bool startWithComma = true;
-    int indentation = 2;
-    labelAndDesc.second.writeCfi(outFile, startWithComma, indentation);
+    if (not shouldWriteUntyped(options)) {
+      outFile << "import FWCore.ParameterSet.Config as cms\n\n";
+      outFile << labelAndDesc.first << " = cms." << baseType << "('" << pluginName << "'";
+    } else {
+      outFile << "import FWCore.ParameterSet.Config as cms\n\n";
 
+      auto pythonName = modifyPluginName(pluginName);
+      outFile << "from ." << pythonName << " import " << pythonName << "\n\n";
+      outFile << labelAndDesc.first << " = " << pythonName << "(";
+      startWithComma = false;
+    }
+    if (not isSameAsDefault) {
+      int indentation = 2;
+      labelAndDesc.second.writeCfi(outFile, startWithComma, indentation, options);
+    }
     outFile << ")\n";
 
     outFile.close();

FWCore/ParameterSet/src/IfExistsDescription.cc
@@ -72,10 +72,15 @@ namespace edm {
     }
   }
 
-  void IfExistsDescription::writeCfi_(
-      std::ostream& os, bool optional, bool& startWithComma, int indentation, bool& wroteSomething) const {
-    node_left_->writeCfi(os, optional, startWithComma, indentation, wroteSomething);
-    node_right_->writeCfi(os, optional, startWithComma, indentation, wroteSomething);
+  void IfExistsDescription::writeCfi_(std::ostream& os,
+                                      bool optional,
+                                      bool& startWithComma,
+                                      int indentation,
+                                      CfiOptions& options,
+                                      bool& wroteSomething) const {
+    node_left_->writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
+    node_right_->writeCfi(os, optional, startWithComma, indentation, options, wroteSomething);
+    parameterMustBeTyped(options);
   }
 
   void IfExistsDescription::print_(std::ostream& os, bool optional, bool writeToCfi, DocFormatHelper& dfh) const {

FWCore/ParameterSet/src/ParameterSetDescription.cc
@@ -125,20 +125,28 @@ namespace edm {
     }
   }
 
-  void ParameterSetDescription::writeCfi(std::ostream& os, bool startWithComma, int indentation) const {
-    using std::placeholders::_1;
+  void ParameterSetDescription::writeCfi(std::ostream& os,
+                                         bool startWithComma,
+                                         int indentation,
+                                         CfiOptions& options) const {
     bool wroteSomething = false;
 
     bool seenWildcard = false;
+    bool seenMultipleWildcards = false;
     for (auto const& entry : entries_) {
       //only add the first seen wildcard to the cfi. This avoids possible ambiguities.
       if (entry.node()->isWildcard()) {
         if (seenWildcard == true) {
+          seenMultipleWildcards = true;
           continue;
         }
         seenWildcard = true;
       }
-      writeNode(entry, os, startWithComma, indentation, wroteSomething);
+      writeNode(entry, os, startWithComma, indentation, options, wroteSomething);
+    }
+
+    if ((anythingAllowed() or seenMultipleWildcards)) {
+      cfi::parameterMustBeTyped(options);
     }
 
     if (wroteSomething) {
@@ -227,10 +235,17 @@ namespace edm {
     }
   }
 
-  void ParameterSetDescription::writeNode(
-      SetDescriptionEntry const& entry, std::ostream& os, bool& startWithComma, int indentation, bool& wroteSomething) {
+  void ParameterSetDescription::writeNode(SetDescriptionEntry const& entry,
+                                          std::ostream& os,
+                                          bool& startWithComma,
+                                          int indentation,
+                                          CfiOptions& options,
+                                          bool& wroteSomething) {
     if (entry.writeToCfi()) {
-      entry.node()->writeCfi(os, entry.optional(), startWithComma, indentation, wroteSomething);
+      entry.node()->writeCfi(os, entry.optional(), startWithComma, indentation, options, wroteSomething);
+    } else {
+      //The simplest way to handle this is to force all items to be full in this PSet
+      cfi::parameterMustBeTyped(options);
     }
   }
 